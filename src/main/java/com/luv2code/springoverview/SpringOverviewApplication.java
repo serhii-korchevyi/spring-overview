64959786f7ba78e9f88bdcdb07b057a1b15bafb6
grumman <grumman@evoplay.tech>
2025-08-06 21:18:29 +0300

    Section 2

--- src/main/java/com/luv2code/springoverview/SpringOverviewApplication.java
+++ src/main/java/com/luv2code/springoverview/SpringOverviewApplication.java
@@ -1,7 +1,13 @@
 package com.luv2code.springoverview;
 
+import com.luv2code.springoverview.dao.StudentDAO;
+import com.luv2code.springoverview.entity.Student;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 /*@SpringBootApplication(
 		scanBasePackages = {"com.luv2code.springoverview",
@@ -12,6 +18,98 @@ public class SpringOverviewApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringOverviewApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
+		return runner -> {
+			// createStudent(studentDAO);
+			createMultipleStudents(studentDAO);
+			// readStudent(studentDAO);
+
+			// queryForStudents(studentDAO);
+			// queryForStudentsByLastName(studentDAO);
+
+			// updateStudent(studentDAO);
+
+			// deleteStudent(studentDAO);
+			// deleteAll(studentDAO);
+		};
+	}
+
+	private void deleteAll(StudentDAO studentDAO) {
+		int deletedRows = studentDAO.deleteAll();
+		System.out.println("deleted rows: " + deletedRows);
+	}
+
+	private void deleteStudent(StudentDAO studentDAO) {
+		int studentId = 7;
+		studentDAO.delete(studentId);
+	}
+
+	private void updateStudent(StudentDAO studentDAO) {
+		int studentId = 1;
+		Student student = studentDAO.findById(studentId);
+
+		student.setFirstName("Scooby");
+
+		studentDAO.update(student);
+
+		System.out.println("Updated student: " + student);
+	}
+
+	private void queryForStudentsByLastName(StudentDAO studentDAO) {
+		List<Student> students = studentDAO.findByLastName("Public");
+
+		for (Student tempStudent : students) {
+			System.out.println(tempStudent);
+		}
+	}
+
+	private void queryForStudents(StudentDAO studentDAO) {
+		List<Student> students = studentDAO.findAll();
+
+		for (Student tempStudent : students) {
+			System.out.println(tempStudent);
+		}
+	}
+
+	private void readStudent(StudentDAO studentDAO) {
+		Student tempStudent = new Student("Daffy", "Duck", "daffy@lub2code.com");
+
+		studentDAO.save(tempStudent);
+
+		int id = tempStudent.getId();
+
+		Student myStudent = studentDAO.findById(id);
+
+		System.out.println("Found the student: " + myStudent);
+	}
+
+	private void createMultipleStudents(StudentDAO studentDAO) {
+		Student tempStudent1 = new Student("John", "Doe", "john@luv2code.com");
+		Student tempStudent2 = new Student("Mary", "Public", "mary@luv2code.com");
+		Student tempStudent3 = new Student("Bonita", "Applebum", "bonita@luv2code.com");
+
+		studentDAO.save(tempStudent1);
+		studentDAO.save(tempStudent2);
+		studentDAO.save(tempStudent3);
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+		// create the student object
+		System.out.println("createStudent");
+		Student tempStudent = new Student("Paul", "Doe", "paul@luv2code.com");
+
+		// save the student object
+		System.out.println("saveStudent");
+		studentDAO.save(tempStudent);
+
+		// display id of the saved student
+		System.out.println("Save student. Generated id: " + tempStudent.getId());
+
+		Student student = studentDAO.findById(tempStudent.getId());
+		System.out.println("Find student by id: " + student);
 	}
 
 }
---
+++ src/main/java/com/luv2code/springoverview/dao/StudentDAOImpl.java
@@ -0,0 +1,72 @@
+package com.luv2code.springoverview.dao;
+
+import com.luv2code.springoverview.entity.Student;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public class StudentDAOImpl implements StudentDAO {
+
+    // define field for entity
+    private EntityManager entityManager;
+
+    // inject entity manager using constructor injection
+    @Autowired
+    public StudentDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    // implement save method
+    @Override
+    @Transactional
+    public void save(Student theStudent) {
+        entityManager.persist(theStudent);
+    }
+
+    @Override
+    public Student findById(Integer id) {
+        return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public List<Student> findAll() {
+        // create query
+        TypedQuery<Student> query = entityManager.createQuery("FROM Student", Student.class);
+
+        // return query results
+        return query.getResultList();
+    }
+
+    @Override
+    public List<Student> findByLastName(String lastName) {
+        TypedQuery<Student> query = entityManager.createQuery("FROM Student WHERE lastName = :lastName", Student.class);
+        query.setParameter("lastName", lastName);
+
+        return query.getResultList();
+    }
+
+    @Override
+    @Transactional
+    public void update(Student student) {
+        entityManager.merge(student);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Integer id) {
+        Student student = entityManager.find(Student.class, id);
+        entityManager.remove(student);
+    }
+
+    @Override
+    @Transactional
+    public int deleteAll() {
+        int deletedRows = entityManager.createQuery("DELETE FROM Student").executeUpdate();
+        return deletedRows;
+    }
+}
